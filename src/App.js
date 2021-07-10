c0bde704c86398faf526ccb360424a2a49c19c61
sarthakkundra <sarthakkundra21@gmail.com>
2021-07-11 00:35:43 +0530

    added pagination

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useEffect, useContext, useState, useCallback } from "react";
 import BankContext from "./context/Bank/BankContext";
 import axios from "axios";
-import { Flex, Box, Select } from "@chakra-ui/react";
-import Table from "./components/Table";
+import { Flex, Box, Select, HStack } from "@chakra-ui/react";
+import { Input } from '@chakra-ui/react';
+import Pagination from './Pagination';
 import "./App.css";
 
 const App = () => {
@@ -26,6 +27,7 @@ const App = () => {
 	} else {
 		return (
 			<>
+      <HStack>
 				<Box w='20%'>
 					<Select
 						onChange={(e) => setState(e.target.value)}
@@ -39,6 +41,10 @@ const App = () => {
 						<option value='HYDERABAD'>Hyderabad</option>
 					</Select>
 				</Box>
+        <Box w="30%">
+          <Input placeholder="search" />
+        </Box>
+        </HStack>
 				<Flex>
 					<Box className='favourites'>
 						<h1>Favourites</h1>
@@ -47,7 +53,7 @@ const App = () => {
 						</ul>
 					</Box>
 					<Box w='50%'>
-						{banks.length > 0 ? <Table banks={banks} /> : <h1>Loading</h1>}
+						{banks.length > 0 && <Pagination data={banks} dataLimit={10} pageLimit={5} />}
 					</Box>
 				</Flex>
 			</>
---
+++ src/Pagination.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import Table from "./components/Table";
+import './styles.css'
+
+function Pagination({ data, RenderComponent, pageLimit, dataLimit }) {
+	const [pages] = useState(Math.round(data.length / dataLimit));
+	const [currentPage, setCurrentPage] = useState(1);
+
+	useEffect(() => {
+		console.log("paginated data");
+		console.log(getPaginatedData());
+	}, []);
+
+	function goToNextPage() {
+		setCurrentPage((page) => page + 1);
+	}
+
+	function goToPreviousPage() {
+		setCurrentPage((page) => page - 1);
+	}
+
+	function changePage(event) {
+		const pageNumber = Number(event.target.textContent);
+		setCurrentPage(pageNumber);
+	}
+
+	function getPaginatedData() {
+		const startIndex = currentPage * dataLimit - dataLimit;
+		const endIndex = startIndex + dataLimit;
+		return data.slice(startIndex, endIndex);
+	}
+
+	function getPaginationGroup() {
+		let start = Math.floor((currentPage - 1) / pageLimit) * pageLimit;
+		return new Array(pageLimit).fill().map((_, idx) => start + idx + 1);
+	}
+
+	return (
+		<div>
+			<Table banks={getPaginatedData()} />
+			<div className='pagination'>
+				{/* previous button */}
+				<button
+					onClick={goToPreviousPage}
+					className={`prev ${currentPage === 1 ? "disabled" : ""}`}>
+					prev
+				</button>
+
+				{/* show page numbers */}
+				{getPaginationGroup().map((item, index) => (
+					<button
+						key={index}
+						onClick={changePage}
+						className={`paginationItem ${
+							currentPage === item ? "active" : null
+						}`}>
+						<span>{item}</span>
+					</button>
+				))}
+
+				{/* next button */}
+				<button
+					onClick={goToNextPage}
+					className={`next ${currentPage === pages ? "disabled" : ""}`}>
+					next
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default Pagination;
